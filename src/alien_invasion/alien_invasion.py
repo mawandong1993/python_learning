737df7c5902e997d7efe25e04de08ed3bd0ff57a
mawandong1993 <mawandong1993@foxmail.com>
2018-07-08 00:57:04 +0800

    更新代码

--- src/alien_invasion/alien_invasion.py
+++ src/alien_invasion/alien_invasion.py
@@ -1,28 +1,27 @@
 import sys
 import pygame
 
+
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
-    screen=pygame.display.set_mode((1280,720))
+    screen = pygame.display.set_mode((1280, 720))
     pygame.display.set_caption("Aline Invasion")
 
-    #设置背景颜色
-    bg_color=(230,230,230)
+    # 设置背景颜色
+    bg_color = (230, 230, 230)
 
-    #开始游戏的主循环
+    # 开始游戏的主循环
     while True:
-        #监听键盘和鼠标事件
+        # 监听键盘和鼠标事件
         for event in pygame.event.get():
-            if event.type==pygame.QUIT:
+            if event.type == pygame.QUIT:
                 sys.exit()
 
-        #每次循环时都重绘屏幕
+        # 每次循环时都重绘屏幕
         screen.fill(bg_color)
 
-        #让最近绘制的屏幕可见
+        # 让最近绘制的屏幕可见
         pygame.display.flip()
 
 run_game()
-
-
