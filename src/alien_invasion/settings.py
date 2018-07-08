a9b33303302b27593c015be04a5bf5bad6032a58
mawandong1993 <mawandong1993@foxmail.com>
2018-07-08 21:19:27 +0800

    更新代码

---
+++ src/alien_invasion/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """储存《外星人入侵》的所有设置的类"""
+
+    def __init__(self):
+        """初始化屏幕的设置"""
+        #屏幕设置
+        self.screen_width=1280
+        self.screen_height=720
+        # 设置背景颜色
+        self.bg_color=(230,230,230)
